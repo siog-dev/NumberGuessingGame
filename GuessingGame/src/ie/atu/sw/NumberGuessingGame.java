469e33f2e1ab5530f85c7f1a2408d3fb37fa0732
siog <siog@LAPTOP-545TJ6PE>
2025-04-04 12:51:55 +0100

    Import Scanner utility and Scanner instance variable for user input

--- GuessingGame/src/ie/atu/sw/NumberGuessingGame.java
+++ GuessingGame/src/ie/atu/sw/NumberGuessingGame.java
@@ -1,7 +1,10 @@
 package ie.atu.sw;
 
+import java.util.Scanner;
+
 public class NumberGuessingGame {
 	private static final int MAX_GUESSES = 12;
+	private Scanner s;
 	
 	public NumberGuessingGame() {
 		
