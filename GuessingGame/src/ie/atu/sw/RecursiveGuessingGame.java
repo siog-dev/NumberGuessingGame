ab6f12664ec43c685d512c135db4a54e2b14e8b2
siog <siog@LAPTOP-545TJ6PE>
2025-04-07 17:55:13 +0100

    Add IF statement

--- GuessingGame/src/ie/atu/sw/RecursiveGuessingGame.java
+++ GuessingGame/src/ie/atu/sw/RecursiveGuessingGame.java
@@ -3,7 +3,18 @@ package ie.atu.sw;
 public class RecursiveGuessingGame {
 	
 	public void guess(int number, int min, int max) {
+		int guess = (int) (min + max) / 2;
 		
+		if (guess == number) {
+			System.out.println("Guessed " + guess + " correctly.");
+			return;
+		}else if (guess > number) {
+			System.out.println("Guessed too high " + guess);
+			max = guess - 1;
+		}else {
+			System.out.println("Guessed too low " + guess);
+			min = guess + 1;
+		}
 	}
 
 	public static void main(String[] args) {
