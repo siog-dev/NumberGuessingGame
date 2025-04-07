ad8b75a1041698ccf13cb4139b6b630df3758cac
siog <siog@LAPTOP-545TJ6PE>
2025-04-07 17:49:20 +0100

    add main method with logic

--- GuessingGame/src/ie/atu/sw/RecursiveGuessingGame.java
+++ GuessingGame/src/ie/atu/sw/RecursiveGuessingGame.java
@@ -1,5 +1,14 @@
 package ie.atu.sw;
 
 public class RecursiveGuessingGame {
+	
+	public void guess(int number, int min, int max) {
+		
+	}
 
+	public static void main(String[] args) {
+		RecursiveGuessingGame rgg = new RecursiveGuessingGame();
+		int number = 767;
+		rgg.guess(number, 0, 1_000_000);
+	}
 }
